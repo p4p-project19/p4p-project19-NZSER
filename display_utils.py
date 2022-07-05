7e6cbb1a608d3c553050f3a59043c9d138ded67e
Harris Tomy <htom380@aucklanduni.ac.nz>
2022-07-05 15:17:14 +1200

    Adds plotting of true and pred. plots + labels

--- display_utils.py
+++ display_utils.py
@@ -9,12 +9,18 @@ def map_w2v_to_quadrant(input_val: float, input_aro: float, input_dom: float) ->
     output_dom = (input_dom - 0.5) * 2
     return output_val, output_aro, output_dom
 
-def quadrant_chart(input_val: float, input_aro: float, xtick_labels=None, ytick_labels=None, ax=None) -> None:
+def quadrant_chart(input_val: list[float], input_aro: list[float], true_val: list[float], true_aro: list[float], xtick_labels=None, ytick_labels=None, ax=None, frame_size: float =.02) -> None:
     '''
     Function to display dimensional emotion data on a quadrant chart
     Function modifed from code provided from: https://gist.githubusercontent.com/ctnormand1/d9326e1556ed199d92063b89675450ef/raw/a707f33883ff59e2f738d236d4c555de56346562/simple_quadrant_chart.py
     '''
-    data = pd.DataFrame({'x': input_val, 'y': input_aro})
+    # Check validity of input values
+    val_size = len(input_val)
+    print(val_size)
+    if val_size != len(input_aro) or val_size != len(true_val) or val_size != len(true_aro):
+        raise ValueError("Input values must be of same length")
+
+    data = pd.DataFrame({'pred_x': input_val, 'pred_y': input_aro, 'true_x': true_val, 'true_y': true_aro})
 
     # Create axes
     ax = plt.axes()
@@ -23,18 +29,34 @@ def quadrant_chart(input_val: float, input_aro: float, xtick_labels=None, ytick_
     ax.set_xlim(-1.2, 1.2)
     ax.set_ylim(-1.2, 1.2)
 
-    ax.scatter(x=data['x'], y=data['y'], c=input_aro, cmap = 'Spectral', edgecolor='darkblue', s=200,
-    zorder=99)
+    # Generate timestamps
+    frame_size_ms = np.arange(0, val_size)
+    
+    # ax.scatter(x=data['x'], y=data['y'], c=input_aro, cmap = 'Spectral', edgecolor='darkblue', s=200, zorder=99)
+    ax.scatter(x=data['true_x'], y=data['true_y'], c=frame_size_ms, cmap='Blues', edgecolor='darkblue', s=20, zorder=99)
+    ax.scatter(x=data['pred_x'], y=data['pred_y'], c=frame_size_ms, cmap='Reds', edgecolor='darkred', s=20, zorder=99)
+
+    # Add annotations for reference
+    ax.annotate('Alarmed', (0, 1.1), fontsize=6)
+    ax.annotate('Distressed', (-.757, .757), fontsize=6)
+    ax.annotate('Miserable', (-1.05, 0), fontsize=6)
+    ax.annotate('Gloomy', (-.757, -.757), fontsize=6)
+    ax.annotate('Sleepy', (0, -1.1), fontsize=6)
+    ax.annotate('Satisfied', (.757, -.757), fontsize=6)
+    ax.annotate('Happy', (1.05, 0), fontsize=6)
+    ax.annotate('Excited', (.757, .757), fontsize=6)
+
     ax.axvline(0, c='k', lw=1)
     ax.axhline(0, c='k', lw=1)
 
 if __name__ == '__main__':
-    
     quadrant_chart(
-        [-1,-.707, 0, .707, 1, .707, -.707, 0, 0],
-        [0, .707, 0, -.707, 0, .707, -.707, 1, -1],
-        xtick_labels=['Low', 'High'],
-        ytick_labels=['Low', 'High']
+        # [-1,-.707, 0, .707, 1, .707, -.707, 0, 0],
+        # [0, .707, 0, -.707, 0, .707, -.707, 1, -1],
+        input_val=[-1, -0.9, -0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1, 0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
+        input_aro=[-1, -0.9, -0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1, 0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
+        true_val=[-1, -0.9, -0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1, 0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
+        true_aro=[-0.9, -0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1, 0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.01],
     )
 
     plt.title('Arousal vs Valence', fontsize=16)
@@ -42,7 +64,3 @@ if __name__ == '__main__':
     plt.xlabel('Valence', fontsize=14)
     plt.grid(True, animated=True, linestyle='--', alpha=0.5)
     plt.show()
-    # plot_color_gradients('Diverging',
-    #                  ['PiYG', 'PRGn', 'BrBG', 'PuOr', 'RdGy', 'RdBu', 'RdYlBu',
-    #                   'RdYlGn', 'Spectral', 'coolwarm', 'bwr', 'seismic'])
-    # plt.show()
