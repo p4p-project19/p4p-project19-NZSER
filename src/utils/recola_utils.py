9a56682fd3a1b8bf0f6688473af952a71675798a
Harris Tomy <htom380@aucklanduni.ac.nz>
2022-07-12 15:00:19 +1200

    Adds loading Recola CSVs -> Dataframes
    Co-authored-by: Arathi Biju <arathibiju@users.noreply.github.com>

---
+++ src/utils/recola_utils.py
@@ -0,0 +1,28 @@
+import os
+import glob
+import pandas as pd
+from librosa import load
+
+# Constants
+SAMPLING_RATE = 16000
+
+def load_recola():
+    """
+    Load the Recola dataset and annotations
+    """
+    file_path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    root = os.path.dirname(os.path.dirname(file_path))
+
+    # Load csv files with annotations
+    aro_csv_files = glob.glob(root + "/data/recola/RECOLA-Annotation/emotional_behaviour/arousal/*.csv")
+    val_csv_files = glob.glob(root + "/data/recola/RECOLA-Annotation/emotional_behaviour/valence/*.csv")
+
+    aro_dfs = []
+    for csv_file in aro_csv_files:
+        aro_df = pd.read_csv(csv_file, delimiter=';')
+        aro_dfs.append(aro_df)
+
+    val_dfs = []
+    for csv_file in val_csv_files:
+        val_df = pd.read_csv(csv_file, delimiter=';')
+        val_dfs.append(val_df)
